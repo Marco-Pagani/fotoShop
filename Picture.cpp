d5f4bc9140d3fa183ab8a36a579d71f07268f3d4
Josh <jtlsc7@gmail.com>
2017-04-10 23:43:13 -0400

    Worked a bit on the Picture files

--- Picture.cpp
+++ Picture.cpp
@@ -1,21 +1,39 @@
 #include <iostream>
-#include <png.h>
+#include <png.h>  //libpng
 #include <stdio.h>
+#include "Picture.h"
 
 using namespace std;
 
-void Picture::readPNGFile(char *fileName) 
-{
+/* I left a massive amount of error handling out of this.  zarb.org/~gc/html/libpng.html*/
+void Picture::readPNGFile(){
 
+	/*Opens file and tests that file is a png.*/
+	png_byte header[8];  //This will take the first 8 bytes of the file.  Libpng uses the first 8 bytes to determine if a file is a png.
+	FILE * file = fopen(fileName, "rb"); //r opens file for reading, b opens it in binary.
+	fread(header, 1, 8, file); //fread(pointer to put stuff in, how many bytes per element, how many elements total, file pointer)
+  if(!png_sig_cmp(header, 0, 8)){ //png_sig_cmp returns false if file is png, true otherwise.  8 = number of bytes read, I dunno what 0 is but I think it will always be 0.
+		isPNG = true;
+		cout << "png_sig_cmp worked" << endl;  //aThis is for bug handling, should be deleted before we finish project.
+	}
 }
 
+/*has errors because I updated variables.*/
+// void Picture::writePNGFile(char *outputFile)
+// {
+// 	FILE *file = fopen(fileName, "wb");
+//
+// 	pngPointer = png_create_write_struct(PNG_LIBPNG_VER_STRING, NULL, NULL, NULL);
+// 	infoPointer = png_create_info_struct(pngPointer);
+//
+//
+// }
 
-void Picture::writePNGFile(char *fileName) 
-{
-	FILE *file = fopen(fileName, "wb");
+int main(){
+	char file[100];  //Will be set to file name.
+	cout << "Please enter your file name.  Include the extension." << endl;
+	cin >> file;
+	Picture picture(file);
 
-	pngPointer = png_create_write_struct(PNG_LIBPNG_VER_STRING, NULL, NULL, NULL);
-	infoPointer = png_create_info_struct(pngPointer);
-
-	
+	return 0;
 }
--- Picture.h
+++ Picture.h
@@ -1,11 +1,26 @@
+#ifndef Picture_h
+#define Picture_h
+#include <png.h>
+
 class Picture {
-private: 
+private:
+	char * fileName;
 	int width, height;
-	png_byte colorType, bitDepth;
-	
-	png_byte *rowPointer;
+	bool isPNG = false;
+	png_byte colorType, bitDepth;  /*colorType is the type of color. (ex. RGB)
+	bitDepth is the amount of bits needed to contain information for
+	1 pixel of an image.  RGB is normally 8. */
+
+	png_bytep *rowPointer;
 
 public:
-	void readPNGFile(char *fileName);
-	void writePNGFile();
+	/*libpng works by reading in the file data, applying the actual image data
+	to an array, and then printing out that array on a separate file.*/
+	Picture(char * inputFile){
+		fileName = inputFile;
+	};
+	void readPNGFile();
+	void writePNGFile(char *outputFile);
 };
+
+#endif
