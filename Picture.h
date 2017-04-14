cf8adac0389d6e1a80963542b181138a22d955a7
Marco Pagani <Marco-Pagani@users.noreply.github.com>
2017-04-14 14:58:13 -0400

    Update Picture.h

--- Picture.h
+++ Picture.h
@@ -4,23 +4,34 @@
 
 class Picture {
 private:
-	char * fileName;
-	int width, height;
 	bool isPNG = false;
-	png_byte colorType, bitDepth;  /*colorType is the type of color. (ex. RGB)
-	bitDepth is the amount of bits needed to contain information for
-	1 pixel of an image.  RGB is normally 8. */
-
-	png_bytep *rowPointer;
+	bool readTrue = true;
+	int width, height;
+	png_byte color_type;
+	png_byte bit_depth;
+	png_bytep * row_pointers;
+	png_structp png_ptr;
+	png_infop info_ptr;
 
 public:
 	/*libpng works by reading in the file data, applying the actual image data
 	to an array, and then printing out that array on a separate file.*/
-	Picture(char * inputFile){
-		fileName = inputFile;
-	};
-	void readPNGFile();
+	Picture(char * inputFile);
+	
+	void convertToHSL(int& r, int& g, int& b, double h, double s, double l);
+	void convertToRGB(double h, double s, double l, int& r, int& g, int& b);
+	
+	void brightness(int value);
+	void contrast(int value);
+	void exposure(int value);
+	void highlights(int value);
+	void shadows(int value);
+	void saturation(int value);
+	void hue(int value);
+	void warmth(int value);
+	
 	void writePNGFile(char *outputFile);
+	
 };
 
 #endif
