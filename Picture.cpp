c5e60545e8eddae0916437faf2a9e5e913257a11
Marco Pagani <Marco-Pagani@users.noreply.github.com>
2017-04-15 19:46:57 -0400

    added algorithms

--- Picture.cpp
+++ Picture.cpp
@@ -37,6 +37,7 @@ void Picture::readPNGFile(char* fileName)
   	if(png_sig_cmp(pngHeader, 0, 8))
   	{ 
     	// ERROR - BREAK FROM CODE
+    	cout << "Not a PNG!" << endl;
     	return;
   	}
 
@@ -49,6 +50,7 @@ void Picture::readPNGFile(char* fileName)
   	if(!png_ptr) 
   	{  
     	// ERROR - BREAK FROM CODE
+    	cout << "Struct not made" << endl;
     	return;
   	}
 
@@ -96,7 +98,12 @@ void Picture::readPNGFile(char* fileName)
 	// Update picture structs now that all checks have been performed to ensure that the picture is in RGB format
 	png_read_update_info(png_ptr, info_ptr);
 
- 
+	if(width != 0 && height != 0){  //prints image width and height.
+    cout << "Width is " << width << "!" << endl;
+    cout << "Height is " << height << "!" << endl;
+  } else {
+	  cout << "Something fucky is going on" << endl;
+  }
   	/* Officially begin to read in the raw picture data from the file */
   	// Allocate memory for the picture data based on its size
   	// Dynamically establish a two-dimensional array to hold each pixel in the file
@@ -140,17 +147,159 @@ void Picture::writePNGFile(char * fileName)
 	
 	png_write_info(png_ptr, info_ptr);  // Write picture information to the file (excluding the pixel data)
 	
+  cout << "got up to png_write_image" << endl;
 	png_write_image(png_ptr, row_pointers);  // Write picture pixel data to the file
 		
+  cout << "got up to png_write_end" << endl;
 	png_write_end(png_ptr, NULL);   // Finishes writing to the end of the file
 
 	// for(int i = 0; i < height; i++){  //Deletes 2d array so no memory leak.  This currently causes the program to crash.
 	//   delete [] row_pointers[i];
 	// }
 	// delete [] row_pointers;
-
+ cout << "got up to fclose()" << endl;
 	fclose(file);   // Closes the file
-	return;
+}
+
+void Picture::convertToHSV(int r, int g, int b, double& hue, double& sat, double& val)
+{
+	float RGB[3] = {r / 255.0, g / 255.0, b / 255.0};
+	float min, max, diff;
+	
+	//find min / max
+	min = RGB[0] < RGB[1] ? RGB[0] : RGB[1];
+	min = min < RGB[2] ? min : RGB[2];
+	
+	max = RGB[0] > RGB[1] ? RGB[0] : RGB[1];
+	max = max > RGB[2] ? max : RGB[2];
+	
+	//find value
+	val = max;
+	
+	//find hue and sat
+	diff = max - min;
+	if(diff < 0.00001){
+		sat = 0;
+		hue = 0;
+		return;
+	}
+	
+	if(max > 0){
+		sat = diff / max;
+	} else {
+		sat = 0;
+		hue = 0;
+		return;
+	}
+	
+	if(RGB[0] == max){
+		hue = (RGB[1] - RGB[2]) / diff;
+	} else if(RGB[1] == max){
+		hue = 2.0 + (RGB[3] - RGB[0]) / diff;
+	} else {
+		hue = 4.0 + (RGB[0] - RGB[1]) / diff;
+	}
+	
+	//clamp hue
+	hue *= 60;
+	if(hue < 0) {
+		hue += 360;
+	}
+  
+}
+
+void Picture::convertToRGB(double hue, double sat, double val, int& r, int& g, int& b)
+{
+	
+  
+  if(sat == 0.0){
+	  r = val;
+	  g = val;
+	  b = val;
+	  return;
+  }
+  
+  double sector = hue;
+  if(sector >= 360) { sector = 0.0; }
+  sector /= 60.0;
+  long rem = (long)sector;
+  double REM = sector - rem;
+  
+  double p = val * (1.0 - sat) * 255;
+  double q = val * (1.0 - (sat * REM)) * 255;
+  double t = val * (1.0 - (sat * (1.0 - REM))) * 255;
+  val *= 255;
+  p += 0.5;
+  q += 0.5;
+  t += 0.5;
+  val += 0.5;
+  
+  switch(rem) {
+	  case 0:
+		r = val;
+		g = t;
+		b = p;
+		break;
+	  case 1:
+		r = q;
+		g = val;
+		b = p;
+		break;
+	  case 2:
+		r = p;
+		g = val;
+		b = t;
+		break;
+	  case 3:
+		r = p;
+		g = q;
+		b = t;
+		break;
+	  case 4:
+		r = t;
+		g = p;
+		b = val;
+		break;
+	  case 5:
+	  default:
+		r = val;
+		g = p;
+		b = q;
+		break;
+  }
+  
+  printf("%i, %i, %i\n", r, g,b);
+}
+
+void Picture::brightness(int value){
+	process(){
+	for (int y=0; y<height; y++) {
+                png_byte* row = row_pointers[y];
+                for (int x=0; x<width; x++) {
+                        png_byte* ptr = &(row[x*4]);
+                        printf("%i, %i, %i\n", ptr[0], ptr[1],ptr[2]);
+                        float h, s, v;
+                        convertToHSV(ptr[0], ptr[1], ptr[2],h,s,v);
+                        //printf("%i, %i, %i, %f, %f, %f \n", ptr[0], ptr[1],ptr[2],h,s,v);
+                        v += (value / 100.0);
+                        if(v < 0)
+							v = 0;
+						if(v > 1)
+							v = 1;
+                        
+						int r = ptr[0];
+						int g = ptr[1];
+						int b = ptr[2];
+                        convertToRGB(h,s,v, r,g,b);
+						ptr[0] = r;
+						ptr[1] = g;
+						ptr[2] = b;
+						//printf("%i, %i, %i, %f, %f, %f \n", ptr[0], ptr[1],ptr[2],h,s,v);
+						
+                }
+        }
+	
+}
 }
 
 int main(){
