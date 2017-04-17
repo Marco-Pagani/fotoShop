a6d15b10aea4e7fb9e8725105a2cddad5bbdefd6
Josh <jtlsc7@gmail.com>
2017-04-16 21:47:29 -0400

    final changes

--- FotoShop01/Picture.cpp
+++ FotoShop01/Picture.cpp
@@ -1,5 +1,5 @@
 #include <iostream>
-#include <stdio.h> 
+#include <stdio.h>
 #include <cstdlib>
 #include <png.h> // libpng library - used for file handling of a png file
 #include <cmath>
@@ -14,7 +14,7 @@ opened (the input must be a character array since the libpng libra
  * ry is written in C and cannot handle strings). The function contains no outpu
  * t. It simply opens the PNG
 file, and creates a Picture object based on the in
- * formation inside the file. 
+ * formation inside the file.
  */
 
 void Interface::set_char(string file_name){
@@ -23,16 +23,16 @@ void Interface::set_char(string file_name){
 
 void Interface::readPNGFile() {
 
-    
+
   // Create an array to read in the first 8 bits of a png file. The first 8 bits represents the header portion for a PNG picture.
   //png_byte header[8];  //This will take the first 8 bytes of the file.  Libpng uses the first 8 bytes to determine if a file is a png.
-  
+
   //Open a file based on the name passed in
   FILE * x = fopen(filep, "rb");  //r opens a file for reading, b opens it in binary.
-  
+
   //Read in the header signature of the PNG file
   //fread(header, 1, 8, x); //fread(pointer to put stuff in, how many bytes per element, how many elements total, file pointer)
-  
+
 
 
   // Check if the file opened is indeed a PNG file
@@ -49,7 +49,7 @@ void Interface::readPNGFile() {
 
   //Initialize the first struct to read in the PNG fule
   png_ptr = png_create_read_struct(PNG_LIBPNG_VER_STRING, NULL, NULL, NULL);  /*Initializing png_struct, one of the structures we need.*/
-  
+
   //Check if the info structure has been properly created
   if(!png_ptr){  //checks to make sure png_struct was created.
     //ERROR - BREAK FROM CODE
@@ -59,17 +59,17 @@ void Interface::readPNGFile() {
 
   // Initalize the second structure to contain the information from the PNG picture
   info_ptr = png_create_info_struct(png_ptr);  /*Initializing png_info, which is the second structure.*/
-  
+
   png_init_io(png_ptr, x);  //Passes file pointer to function that sets up input code.  Needs to use fread.
-  
+
   //png_set_sig_bytes(png_ptr, 8);  //png_sig_cmp took bytes from the start of the file, this gives them back to the file.
   //png_set_user_limits(png_ptr, width_max, heigth_max);  //Used to set limits on png input size.  It defaults to 1 million by 1 million.
-  
+
   png_read_info(png_ptr, info_ptr);  //Reads all file information except actual image data.
   // if(color_type == PNG_COLOR_TYPE_PALETTE){  //Expands paletted images to RGB?
   //   png_set_palette_to_rgb(png_ptr);
   // }
- 
+
 
   // Once the picture information has been read into the program - a Picture object can be established based on the information from the picture
   width = png_get_image_width(png_ptr, info_ptr);  //Gets image width
@@ -101,7 +101,7 @@ void Interface::readPNGFile() {
     png_set_tRNS_to_alpha(png_ptr);
   }
 
-   
+
   if(color_type == PNG_COLOR_TYPE_RGB ||
      color_type == PNG_COLOR_TYPE_GRAY ||
      color_type == PNG_COLOR_TYPE_PALETTE){
@@ -139,16 +139,16 @@ void Interface::readPNGFile() {
   }
 
   fclose(x);
-} 
+}
 
 /* This function opens up a file and writes a PNG file to it. The method takes in a character array as input that will be used to represent the file name (a
 character array must be used instead of a string because libpng is written in C and cannot function with strings). The function contains no output.*/
 void Interface::writePNGFile() {
-    
+
     // Open up a new file that will be used to write a PNG picture to
     //http://www.libpng.org/pub/png/book/chapter15.html was of especial use.
     FILE * output = fopen(filep, "wb"); //Creates new file to write to.
-    
+
 	/*Two structures need to be created as part of the libpng library. One structure will be used to hold the information from the picture and the
 	other structure will be used to write the information to the file.*/
 
@@ -156,13 +156,13 @@ void Interface::writePNGFile() {
 
 
     png_ptr       = png_create_write_struct(PNG_LIBPNG_VER_STRING, NULL, NULL, NULL); //Creates png and info struct for writing.
-    
+
     // Establish the struct for handling the information of the PNG image
     info_ptr      = png_create_info_struct(png_ptr);
-    
+
 
     png_init_io(png_ptr, output); //Initializes output.
-    
+
     // Set basic parameters for the image
     png_set_IHDR(
         png_ptr,
@@ -175,23 +175,23 @@ void Interface::writePNGFile() {
         PNG_COMPRESSION_TYPE_DEFAULT,
         PNG_FILTER_TYPE_DEFAULT
     ); //Sets basic image parameters.
-    
+
     png_write_info(png_ptr, info_ptr); //Writes all non-image data to file.
-       
+
     png_write_image(png_ptr, row_pointers); //Writes image data to file.
-    
+
     png_write_end(png_ptr, NULL); //Finishes writing file.  Change NULL to the info pointer if you want comments or time.
-    
+
     // for(int i = 0; i < height; i++){  Deletes 2d array so no memory leak.  This
     // currently causes the program to crash.   delete [] row_pointers[i]; } delete
     // [] row_pointers;
-   
+
     fclose(output);   // Close the file
 }
 
 
 /* Function for converting the picture information from a red-green-blue format to a hue-saturation-value format. The method takes in three integers to represent
-the red, blue, and green value from the picture. It contains references to three doubles so that the address for hue, saturation, and value can be adjusted. The 
+the red, blue, and green value from the picture. It contains references to three doubles so that the address for hue, saturation, and value can be adjusted. The
 function has no output after performing the conversion. */
 void Interface::convertToHSV(int r, int g, int b, double &h, double &s, double &v)
 {
@@ -208,10 +208,10 @@ void Interface::convertToHSV(int r, int g, int b, double &h, double &s, double &
   	double hue = 0.0;
   	double sat = 0.0;
   	double val = 0.0;
-  
+
   	// Create an array to hold the red, green, and blue values from the pixel
   	double RGB [3] = {rP, gP, bP};
-  
+
   	// Convert each red, green, blue value to have 8-bit color depth
   	int maxInd;   // Initalize a variable to keep track of the index for which value is the largest between red, green, or blue
   	for (int i=0; i < 3; i++)
@@ -219,7 +219,7 @@ void Interface::convertToHSV(int r, int g, int b, double &h, double &s, double &
   		// Check which red, green, and blue value is the smallest of the three
     	if (RGB[i] < min)
       		min = RGB[i];
-      
+
       	// Check which red, green, and blue value is the largest of the three
     	if (RGB[i] > max)
     	{
@@ -227,15 +227,15 @@ void Interface::convertToHSV(int r, int g, int b, double &h, double &s, double &
       		maxInd = i;   // Update the value of the index that contains the largest value
     	}
   	}
-  
+
   	double delta = max - min;
 
   	/* FORMULA FOR CALCULATING HUE*/
-  
+
   	// Check if the value for delta is a miniscule value - in which case, it can be set to 0
   	if(delta < 0.000001)
     	hue = 0.0;
-  
+
   	// Else, check if the maximum value is from red index
   	else if(maxInd == 0)
   	{
@@ -243,27 +243,27 @@ void Interface::convertToHSV(int r, int g, int b, double &h, double &s, double &
 
         while(mod_factor <= 0.0)
             mod_factor = mod_factor + 6.0;
-    	
+
         while(mod_factor >= 6.0)
       		mod_factor = mod_factor - 6.0;
-  
+
     	hue = mod_factor;
       }
-  
+
   	// Else, check if the maximum value is from the green index
   	else if(maxInd == 1)
     	hue = (bP - rP)/delta + 2.0;
-  	
+
   	// Else, check if the maximum value is from the blue index
   	else if(maxInd == 2)
     	hue = (rP - gP)/delta + 4.0;
-  	
+
   	// Else, produce an error
   	else
     	cout << "error" << endl;
-  
+
   	hue = hue * 60.0;
-  
+
   	/* FORMULA FOR CALCULATING SATURATION */
 
   	// If the maximum value is a miniscule value, then it can be set to 0
@@ -272,11 +272,11 @@ void Interface::convertToHSV(int r, int g, int b, double &h, double &s, double &
     // Else, set the value for saturation
   	else
     	sat = delta/max;
-   
+
     /* FORMULA FOR CALCULATING VALUE */
 
  	val = max;   // Set value
-  
+
   	// Re-establish the values to the address assigned for hue, saturation, and value based on the values calculated through the formulas
   	h = hue;
   	s = sat;
@@ -293,9 +293,9 @@ void Interface::convertToRGB(double hue, double sat, double val, int &r, int &g,
     mod_factor = mod_factor + 2.0;
   double x = c * (1 - abs(mod_factor - 1));
   double m = val - c;
-  
+
   double rP, gP, bP;
-#include "Picture.h"
+  
   if((0.0 <= hue && hue < 60.0)){
       rP = c;
       gP = x;
@@ -326,7 +326,7 @@ void Interface::convertToRGB(double hue, double sat, double val, int &r, int &g,
       gP = 0;
       bP = x;
   }
-  
+
   r = (int)((rP + m) * 255.0);
   g = (int)((gP + m) * 255.0);
   b = (int)((bP + m) * 255.0);
@@ -398,7 +398,7 @@ void Interface::changeSaturation(int value) {
 //can read in either a double directly or take in an int and process it inside, makes no difference
 void Interface::changeBrightness(int value){
   if(value > 100 || value < -100){
-  	//WILL MOST LIKELY HAVE TO TAKE OUT 
+  	//WILL MOST LIKELY HAVE TO TAKE OUT
     //cout << "INVALID" << endl;
     return;
   }
@@ -425,7 +425,7 @@ void Interface::changeBrightness(int value){
       ptr[2] = b;
     }
   }
-	
+
 }
 
 
@@ -446,15 +446,15 @@ void Interface::changeContrast(int value){
       r = ptr[0];
       g = ptr[1];
       b = ptr[2];
-      
+
       r = (int)(cFactor*(r - 128.0) + 128);
       g = (int)(cFactor*(g - 128.0) + 128);
       b = (int)(cFactor*(b - 128.0) + 128);
-      
+
       ptr[0] = clamp(r);
       ptr[1] = clamp(g);
       ptr[2] = clamp(b);
-      
+
     }
   }
 }
@@ -593,14 +593,14 @@ void Interface::rotateLeft(){
   temp = width;
   width = height;
   height = temp;
-  
+
   //Creates a new array of png_bytes to replace previous, using the new hieght and widths
   png_bytep * copy_pointers = (png_bytep*)malloc(sizeof(png_bytep) * height);
   for(int i = 0; i < height; i++){
     copy_pointers[i] = (png_byte*)malloc(width * 4);//The malloc in the read method is essentially this
   }
-  
-  
+
+
   for(int y = 0; y < height; y++) {
     png_bytep copy = copy_pointers[y];
     for(int x = 0; x < width; x++) {
@@ -611,16 +611,16 @@ void Interface::rotateLeft(){
       pc[1] = px[1];
       pc[2] = px[2];
       pc[3] = px[3];
-    }   
+    }
   }
-  
+
   for(int y = 0; y < width; y++) {
     free(row_pointers[y]);
   }
   free(row_pointers);
-  
+
   row_pointers = copy_pointers;
-  
+
   verticalFlip();
 }
 
@@ -630,14 +630,14 @@ void Interface::rotateRight(){
   temp = width;
   width = height;
   height = temp;
-  
+
   //makes a copy of the png_byte array to store rotated data
   png_bytep * copy_pointers = (png_bytep*)malloc(sizeof(png_bytep) * height);
   for(int i = 0; i < height; i++){
     copy_pointers[i] = (png_byte*)malloc(width * 4);
   }
-  
-  
+
+
   for(int y = height-1; y >= 0; y--) {
     png_bytep copy = copy_pointers[y];
     for(int x = width-1; x >= 0; x--) {
@@ -648,17 +648,17 @@ void Interface::rotateRight(){
       pc[1] = px[1];
       pc[2] = px[2];
       pc[3] = px[3];
-    }   
+    }
   }
-  
+
   for(int y = 0; y < width; y++) {
     free(row_pointers[y]);
   }
   free(row_pointers);
-  
+
   row_pointers = copy_pointers;
-  
-  verticalFlip();  
+
+  verticalFlip();
 }
 
 void Interface::horizontalFlip(){
